b4e24b1c68023d3c4f63c5f7e1e64edff43ca44b
Corniel Nobel <mail@corniel.nl>
2019-11-26 14:47:09 +0100

    Initial setup

---
+++ src/Qowaiv.CodeGenerator/SingleValueObjectAttribute.cs
@@ -0,0 +1,23 @@
+﻿using CodeGeneration.Roslyn;
+using System;
+using System.Diagnostics;
+
+namespace Qowaiv.CodeGenerator
+{
+    [AttributeUsage(AttributeTargets.Struct, AllowMultiple = false)]
+    [CodeGenerationAttribute(typeof(SvoCodeGenerator))]
+    [Conditional(CodeGeneration)]
+    public  sealed class SingleValueObjectAttribute : Attribute
+    {
+        private const string CodeGeneration = nameof(CodeGeneration);
+
+        public SingleValueObjectAttribute(Type underlyingType, string fullName)
+        {
+            UnderlyingType = underlyingType ?? typeof(string);
+            FullName = fullName ?? UnderlyingType.Name;
+        }
+
+        public Type UnderlyingType { get; }
+        public string FullName { get; }
+    }
+}
---
+++ src/Qowaiv.CodeGenerator/SvoCodeGenerator.cs
@@ -0,0 +1,41 @@
+﻿using CodeGeneration.Roslyn;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Qowaiv.CodeGenerator
+{
+    public class SvoCodeGenerator: ICodeGenerator
+    {
+        public SvoCodeGenerator(AttributeData attributeData)
+        {
+        }
+
+        public Task<SyntaxList<MemberDeclarationSyntax>> GenerateAsync(TransformationContext context, IProgress<Diagnostic> progress, CancellationToken cancellationToken)
+        {
+            if (!(context.ProcessingNode is TypeDeclarationSyntax typeDeclaration))
+            {
+                return Task.FromResult(SyntaxFactory.List<MemberDeclarationSyntax>());
+            }
+
+            if (typeDeclaration is StructDeclarationSyntax structDeclaration)
+            {
+                var partial = StructPartial(structDeclaration);
+                var results = SyntaxFactory.SingletonList(partial);
+                return Task.FromResult(results);
+            }
+
+            return Task.FromResult(SyntaxFactory.List(Array.Empty<MemberDeclarationSyntax>()));
+        }
+
+        private static MemberDeclarationSyntax StructPartial(StructDeclarationSyntax declaration)
+        {
+            return SyntaxFactory.StructDeclaration(declaration.Identifier)
+                .WithTypeParameterList(declaration.TypeParameterList)
+                .WithModifiers(SyntaxTokenList.Create(SyntaxFactory.Token(SyntaxKind.PartialKeyword)));
+        }
+    }
+}
